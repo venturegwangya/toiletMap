102583f4ec0ef89b5cdf96d0c5b556e1f791a4ad
pizzakinggod <mgparkprint@gmail.com>
2020-09-30 00:48:28 +0900

    (추가) styled-components 추가

--- src/view/App.tsx
+++ src/view/App.tsx
@@ -1,7 +1,17 @@
 import * as React from 'react';
+import styled from 'styled-components';
 
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 export default class App extends React.Component {
   public render() {
-    return <div>Hello World</div>;
+    return (
+      <div>
+        <Title>Hello World</Title>
+      </div>
+    );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -23,6 +23,7 @@ module.exports = {
             '@babel/preset-react',
             '@babel/preset-typescript',
           ],
+          plugins: ['babel-plugin-styled-components', {}],
         },
       },
     ],
