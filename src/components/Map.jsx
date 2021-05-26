37e39019aa230630bf618b2959410cb596975cbf
JunHee Lee <jhlee910609@gmail.com>
2021-05-27 01:20:46 +0900

    카카오 api 추가

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,10 +1,29 @@
 /** @jsxImportSource @emotion/react */
 import { css } from '@emotion/react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { useEffect } from 'react';
 
 const seoul = [37.40095, 126.733522];
 
 function Map() {
+  useEffect(() => {
+    // effect
+    // 장소 검색 객체 생성
+    const { kakao } = window;
+    const ps = new kakao.maps.services.Places();
+
+    // 키워드로 장소 검색
+    // 키워드 및
+    ps.keywordSearch('개방화장실', (data, status, pagination) => {
+      console.log(data);
+      console.log(status);
+      console.log(pagination);
+      // 이거 한 후에 현재 위치와 비교해서 marker 표시해줘야할듯
+    });
+    return () => {
+      // cleanup
+    };
+  }, []);
   return (
     <MapContainer
       css={css`
