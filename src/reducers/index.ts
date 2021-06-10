7f88da3e9fbf78cff6786ba96d4a735265fbd78d
JunHee Lee <jhlee910609@gmail.com>
2021-06-10 19:38:59 +0900

    라우팅 제거 후, state로 Path 관리

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
 import mapReducer from './mapReducer';
+import pathReducer from './pathReducer';
 
 export const rootReducer = combineReducers({
   mapReducer,
+  pathReducer,
 });
