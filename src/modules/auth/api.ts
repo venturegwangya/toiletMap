abbb0f0f6a1a664d6f609652920fec1120594c28
JunHee Lee <jhlee910609@gmail.com>
2021-08-05 19:55:27 +0900

    주석 추가

--- src/modules/auth/api.ts
+++ src/modules/auth/api.ts
@@ -2,6 +2,9 @@
 import firebase from 'firebase/app';
 import { firebaseAuth } from '../configureFirebase';
 
+/**
+ * @override 회원가입
+ */
 export function signUpWithEmailAndPassword(
   email: string,
   password: string,
@@ -14,6 +17,9 @@ export function signUpWithEmailAndPassword(
     .catch(error => alert(error.message));
 }
 
+/**
+ * @override 로그인
+ */
 export function signInWithEmailAndPassword(email: string, password: string) {
   if (email.length === 0 || password.length === 0) return;
   firebaseAuth
@@ -33,6 +39,9 @@ export function subscribeToAuthChange(
   });
 }
 
+/**
+ * @override 로그아웃
+ */
 export function logout() {
   firebaseAuth.signOut();
 }
