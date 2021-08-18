5057ace17a79b803e3783bd4132770d8a0feaefb
vlwkaos <44766242+vlwkaos@users.noreply.github.com>
2021-08-19 02:33:01 +0900

    Revert "Firebase local emulator suite 추가"

--- src/firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY ?? 'dummy_api_key',
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN ?? 'dummy_auth_domain',
-  projectId: process.env.FIREBASE_PROJECT_ID ?? 'dummy_project_id',
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET ?? 'dummy_storage_bucket',
-  messagingSenderId:
-    process.env.FIREBASE_MESSAGING_SENDER_ID ?? 'dummy_messaging_sender_id',
-  appId: process.env.FIREBASE_APP_ID ?? 'dummy_app_id',
-};
