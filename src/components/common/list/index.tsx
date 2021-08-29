17a141a5d8c40e38b8621ba5078c81be32b1c3db
vlwkaos <44766242+vlwkaos@users.noreply.github.com>
2021-08-30 01:31:06 +0900

    Fix scroll protruding round container (#120)

--- src/components/common/list/index.tsx
+++ src/components/common/list/index.tsx
@@ -1,6 +1,14 @@
+import { ReactNode } from 'react';
 import tw from 'twin.macro';
 
-export const DividedList = tw.ul`divide-y divide-gray-300`;
-export const RoundDividedList = tw(
-  DividedList,
-)`w-full p-8 overflow-y-scroll bg-white rounded-xl shadow-default md:(margin-top[1em] margin-bottom[1em])`;
+export const DividedList = tw.ul`h-full p-8 divide-y divide-gray-300 overflow-y-auto scrollbar-width[1em]`;
+export const RoundContainer = tw.div`w-full overflow-hidden bg-white rounded-xl shadow-default md:(margin-top[1em] margin-bottom[1em])`;
+export const RoundDividedList = ({
+  children,
+}: {
+  children: ReactNode;
+}): JSX.Element => (
+  <RoundContainer>
+    <DividedList>{children}</DividedList>
+  </RoundContainer>
+);
